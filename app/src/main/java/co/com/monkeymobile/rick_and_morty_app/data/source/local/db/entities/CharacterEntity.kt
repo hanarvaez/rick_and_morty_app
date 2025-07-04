0481ba24135dd0785d634729a62437a5622509f8
Hector A. Narvaez <8546634+hanarvaez@users.noreply.github.com>
2025-07-03 21:06:38 -0500

    Add entities for local database

---
+++ app/src/main/java/co/com/monkeymobile/rick_and_morty_app/data/source/local/db/entities/CharacterEntity.kt
@@ -0,0 +1,19 @@
+package co.com.monkeymobile.rick_and_morty_app.data.source.local.db.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+const val CHARACTERS_TABLE_NAME = "characters"
+
+const val CHARACTERS_TABLE_ID = "id"
+
+@Entity(tableName = CHARACTERS_TABLE_NAME)
+data class CharacterEntity(
+    @PrimaryKey @ColumnInfo(name = CHARACTERS_TABLE_ID) val id: Int,
+    val image: String,
+    val name: String,
+    val species: String,
+    val gender: String,
+    val status: String
+)
